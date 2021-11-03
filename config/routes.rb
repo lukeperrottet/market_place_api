e122d94c69954e74b50871d7961f91786031dd27
lukeperrottet <luke.perrottet@qoqa.com>
2021-11-03 15:52:12 +0100

    Create user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Api definition
-  namespace _api, defaults: { format: :json } do
+  namespace :api, defaults: { format: :json } do
     namespace :v1 do
 
     end
