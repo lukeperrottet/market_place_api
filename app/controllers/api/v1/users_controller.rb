19bbf6a8cb03c11feb7cc03173e69683f30f9959
lukeperrottet <luke.perrottet@qoqa.com>
2021-11-08 08:11:18 +0100

    Adds udpate action the users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :set_user, only: %i[show update]
+
   def show
-    render json: User.find(params[:id])
+    render json: @user
   end
 
   # POST /users
@@ -14,10 +16,22 @@ class Api::V1::UsersController < ApplicationController
     end
   end
 
+  def update
+    if @user.update(user_params)
+      render json: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
   private
 
   # Only allow a trusted parameter "white list" though.
   def user_params
     params.require(:user).permit(:email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
